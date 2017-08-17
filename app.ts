7478f8247c71807653ddbc12f0f4d8570af0fc6b
Anderson <andersontravassos1978@gmail.com>
2017-08-17 17:16:31 -0300

    teste

--- app.ts
+++ app.ts
@@ -1,2 +1,8 @@
 let message:string="help, i don't know how make this"
 console.log(message);
+
+let episode:number=4
+episode=4
+console.log(episode)
+episode=episode +1
+console.log("proximo é" + episode)
--- dist/app.js
+++ dist/app.js
@@ -1,5 +1,7 @@
 var message = "help, i don't know how make this";
 console.log(message);
 var episode = 4;
-episode = '4';
+episode = 4;
 console.log(episode);
+episode = episode + 1;
+console.log("proximo é" + episode);
